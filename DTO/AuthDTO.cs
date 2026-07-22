20b93ada09aecb9c733bbd79387028f3f544aba0
Junel Fronda <junel.fronda@dxc.com>
2026-07-22 23:27:14 +0800

    Add minimum password length

--- DTO/AuthDTO.cs
+++ DTO/AuthDTO.cs
@@ -6,6 +6,7 @@ namespace task_list.server.DTO;
 public class RegisterDto
 {
     public string Username { get; set; } = string.Empty;
+    [Required, MinLength(6)]
     public string Password { get; set; } = string.Empty;
 }
 
